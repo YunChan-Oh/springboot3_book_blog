f7d8b51a7c2ebd285dbc94c6c5ec761a66f9e052
YunChan-Oh <oyunchan9@gmail.com>
2024-02-06 21:55:31 +0900

    블로그-1 , article엔티티 롬복 사용 O

--- src/main/java/me/ohyunchan/springbootdeveloper/domain/Article.java
+++ src/main/java/me/ohyunchan/springbootdeveloper/domain/Article.java
@@ -1,9 +1,14 @@
 package me.ohyunchan.springbootdeveloper.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //1씩 증가
@@ -20,21 +25,5 @@ public class Article {
     public Article(String title, String content) {
         this.title = title;
         this.content = content;
-    }
-
-    protected Article(){ //기본 생성자
-    }
-
-    //게터
-    public Long getId(){
-        return id;
-    }
-
-    public String getTitle(){
-        return title;
-    }
-
-    public String getContent(){
-        return content;
     }
 }
