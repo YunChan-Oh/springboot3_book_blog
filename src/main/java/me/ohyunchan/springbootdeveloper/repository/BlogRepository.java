4596a9735092705ff8dc6adeee0b5d152692a834
YunChan-Oh <oyunchan9@gmail.com>
2024-02-06 22:47:48 +0900

    블로그-1 , save 로직 완성

---
+++ src/main/java/me/ohyunchan/springbootdeveloper/repository/BlogRepository.java
@@ -0,0 +1,7 @@
+package me.ohyunchan.springbootdeveloper.repository;
+
+import me.ohyunchan.springbootdeveloper.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article,Long> {
+}
---
+++ src/main/java/me/ohyunchan/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,18 @@
+package me.ohyunchan.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.ohyunchan.springbootdeveloper.domain.Article;
+import me.ohyunchan.springbootdeveloper.dto.AddArticleRequest;
+import me.ohyunchan.springbootdeveloper.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor //final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+@Service //빈 등록
+public class BlogService {
+    private final BlogRepository blogRepository;
+
+    //블로그 글 추가 메서드
+    public Article save(AddArticleRequest request){
+        return blogRepository.save(request.toEntity());
+    }
+}
