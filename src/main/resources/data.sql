6d4540265c040963dbe136abc8160006024405ab
YunChan-Oh <oyunchan9@gmail.com>
2024-02-08 18:39:07 +0900

    블로그-2 , 엔티티 생성,수정 시간 자동 업로드 코드

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-Insert into article (title,content) values ('제목 1','내용 1')
-Insert into article (title,content) values ('제목 2','내용 2')
-Insert into article (title,content) values ('제목 3','내용 3')
+Insert into article (title,content,created_at,updated_at) values ('제목 1','내용 1',now(),now())
+Insert into article (title,content,created_at,updated_at) values ('제목 2','내용 2',now(),now())
+Insert into article (title,content,created_at,updated_at) values ('제목 3','내용 3',now(),now())
